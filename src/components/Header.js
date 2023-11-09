e2ac515e4e19f482620cbd27ee14f225c3e3dea9
songganam <dev.ganam@gmail.com>
2023-11-09 14:52:37 +0900

    feat : emotion, styled components 사용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
+import { HeaderTopLeft } from "../styled/header";
 import "../styles/header.css";
 
 function Header() {
   // js 코드 자리
+
   return (
     <header className="header">
       {/* <!-- 레이아웃 --> */}
       <div className="header-inner">
         <div className="header-top">
-          <div className="header-top-left">
+          <HeaderTopLeft w="625" h="50">
             <a href="index.html" className="logo"></a>
 
             {/* <!-- 검색은 div 하나 주자 --> */}
@@ -20,7 +22,7 @@ function Header() {
                 <input type="button" value="검색" className="search-bt" />
               </form>
             </div>
-          </div>
+          </HeaderTopLeft>
 
           <div className="header-top-right">
             <ul className="member-menu">
